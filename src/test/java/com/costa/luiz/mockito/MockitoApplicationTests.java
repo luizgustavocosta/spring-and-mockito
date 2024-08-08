d94e5c016e800ea138ce0efe39b379ed09cabc00
Luiz Gustavo <luiz.gustavo.costa82@gmail.com>
2024-08-07 22:54:33 -0300

    Initial commit

---
+++ src/test/java/com/costa/luiz/mockito/MockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.costa.luiz.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
